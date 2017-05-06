7d6be15a9f9609fe62ad0e9b12ed0a12482df75a
Tim Smith <tsontario@gmail.com>
2017-05-06 14:06:04 -0400

    Fetches all pages of JSON and terminates loop properly. Begin refactoring into smaller methods

--- cookies.go
+++ cookies.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strconv"
 )
 
 // Custom structs for nested info
@@ -33,33 +34,53 @@ type JSONInfo struct {
 	Pagination        PageInfo
 }
 
+type JSONOutput struct {
+	Remaining_cookies  int64
+	Unfulfilled_orders []int64
+}
+
 func main() {
 
 	// order info
 	var available_cookies int64
 	var orders []Order
 
-	resp, err := http.Get("https://backend-challenge-fall-2017.herokuapp.com/orders.json")
-	if err != nil {
-		fmt.Printf("%s\n", err)
-	}
-	htmlData, err := ioutil.ReadAll(resp.Body)
+Process:
+	for page := 1; ; page++ {
+		// Get current page
+		// TODO functionify to pageData := getPage(page int)
+		resp, _ := getPage(page)
+		htmlData, _ := ioutil.ReadAll(resp.Body)
 
-	var obj JSONInfo
-	error := json.Unmarshal(htmlData, &obj)
+		var obj JSONInfo
+		error := json.Unmarshal(htmlData, &obj)
 
-	if error != nil {
-		panic(error)
-	}
+		if error != nil {
+			panic(error)
+		}
 
-	available_cookies = obj.Available_cookies
-
-	for pagesToProcess {
+		available_cookies = obj.Available_cookies
 
 		for _, v := range obj.Orders {
 			orders = append(orders, v)
 		}
+
 		fmt.Println(available_cookies)
 		fmt.Println(orders)
+
+		if obj.Pagination.Current_page*obj.Pagination.Per_page > obj.Pagination.Total {
+			break Process
+		}
+	} // end Process
+}
+
+func getPage(pageNum int) (resp *http.Response, err error) {
+	page := strconv.Itoa(pageNum)
+	url := "https://backend-challenge-fall-2017.herokuapp.com/orders.json?page=" + page
+	fmt.Println(url)
+	resp, err = http.Get(url)
+	if err != nil {
+		fmt.Printf("%s\n", err)
 	}
+	return resp, err
 }
