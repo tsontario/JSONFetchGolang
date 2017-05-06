4cf465a544e3a564d92274ded605e234064c4b05
Tim Smith <tsontario@gmail.com>
2017-05-06 16:12:52 -0400

    Fulfill all orders that don't contain cookies

--- cookies.go
+++ cookies.go
@@ -7,9 +7,12 @@ import (
 	"io/ioutil"
 	"net/http"
 	"strconv"
+	//"strings"
 )
 
-// Custom structs for nested info
+var URL = "https://backend-challenge-fall-2017.herokuapp.com/orders.json?page="
+
+// Structs for JSON unmarshalling
 type Product struct {
 	Title  string
 	Amount int64
@@ -39,6 +42,15 @@ type JSONOutput struct {
 	Unfulfilled_orders []int64
 }
 
+func (order Order) Contains(food string) bool {
+	for _, product := range order.Products {
+		if product.Title == food {
+			return true
+		}
+	}
+	return false
+}
+
 func main() {
 
 	// order info
@@ -47,40 +59,49 @@ func main() {
 
 Process:
 	for page := 1; ; page++ {
-		// Get current page
-		// TODO functionify to pageData := getPage(page int)
 		resp, _ := getPage(page)
 		htmlData, _ := ioutil.ReadAll(resp.Body)
 
-		var obj JSONInfo
-		error := json.Unmarshal(htmlData, &obj)
+		var jsonData JSONInfo
+		error := json.Unmarshal(htmlData, &jsonData)
 
 		if error != nil {
 			panic(error)
 		}
 
-		available_cookies = obj.Available_cookies
-
-		for _, v := range obj.Orders {
+		for _, v := range jsonData.Orders {
 			orders = append(orders, v)
 		}
 
-		fmt.Println(available_cookies)
-		fmt.Println(orders)
-
-		if obj.Pagination.Current_page*obj.Pagination.Per_page > obj.Pagination.Total {
+		if jsonData.Pagination.Current_page*jsonData.Pagination.Per_page > jsonData.Pagination.Total {
+			available_cookies = jsonData.Available_cookies
 			break Process
 		}
 	} // end Process
+
+	fulfillNoCookieOrders(orders)
+	fmt.Println(available_cookies)
+	fmt.Println(orders)
 }
 
 func getPage(pageNum int) (resp *http.Response, err error) {
 	page := strconv.Itoa(pageNum)
-	url := "https://backend-challenge-fall-2017.herokuapp.com/orders.json?page=" + page
-	fmt.Println(url)
+	url := URL + page
+
 	resp, err = http.Get(url)
+
 	if err != nil {
 		fmt.Printf("%s\n", err)
 	}
 	return resp, err
+}
+
+func fulfillNoCookieOrders(orders []Order) {
+	for i := 0; i < len(orders); i++ {
+		if !orders[i].Contains("Cookie") {
+			// If the order doesn't contain a cookie we set it to fulfilled
+			orders[i].Fulfilled = true
+			fmt.Printf("Fulfilled order %d.\n", orders[i].Id)
+		}
+	}
 }
